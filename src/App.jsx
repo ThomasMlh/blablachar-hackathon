de7cfad69d56a4fea8096b51a8871e8c17eda5c1
Thomas Druart <thomas.druart.59@gmail.com>
2020-11-19 22:10:41 +0100

    router init

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
 import { useEffect, useState } from "react";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import FrontPage from "./components/FrontPage";
+import Drivers from "./components/Drivers";
+import Form from "./components/Form";
+import ChariotPage from "./components/ChariotPage";
 
 import { getProfiles } from "./api/apiCall";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   const [profiles, setProfiles] = useState([]);
@@ -22,7 +25,12 @@ function App() {
   return (
     <div>
       <Header />
-      <FrontPage />
+      <Switch>
+        <Route path="/post" component={Form} />
+        <Route path="/drivers" component={Drivers} />
+        <Route path="/chariots" component={ChariotPage} />
+        <Route exact path="/" component={FrontPage} />
+      </Switch>
       <Footer />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,94 +1,95 @@
 import styled from "styled-components";
-import {device} from "./Device"
+import { device } from "./Device";
+import { Link } from "react-router-dom";
 
 export const DivHeader = styled.div`
   background-color: #efdcbe;
   display: flex;
-  justify-content : space-around;
+  justify-content: space-around;
   align-items: center;
-  color : white;
+  color: white;
   font-size: 4vh;
   text-shadow: 1px 1px 2px black;
   height: 12vh;
   @media ${device.mobile} {
     height: 15vh;
     font-size: 17px;
-    }
-
+  }
 `;
 
 export const DivLogo = styled.div`
-display : flex;
-padding-right: 18vw;
-@media ${device.mobile} {
+  display: flex;
+  padding-right: 18vw;
+  @media ${device.mobile} {
     padding-right: 0vw;
-
-    }
+  }
 `;
 export const ImgLogo = styled.img`
-height: 20vh;
-@media ${device.mobile} {
+  height: 20vh;
+  @media ${device.mobile} {
     height: 12vh;
-    width: 12vh
-    }`
+    width: 12vh;
+  }
+`;
 
-export const A = styled.a`
-text-decoration: none;
-padding-right: 3vw;
-color : white;
-&:hover{
-    color:black;
-    transition: 0.6s;}
-    @media ${device.mobile} {
-        padding-right: 0vw;
-    }
-
-`
+export const A = styled(Link)`
+  text-decoration: none;
+  padding-right: 3vw;
+  color: white;
+  &:hover {
+    color: black;
+    transition: 0.6s;
+  }
+  @media ${device.mobile} {
+    padding-right: 0vw;
+  }
+`;
 
 export const PLogo = styled.p`
-text-align: center;
-font-size: 4vh;
-padding-left: 2vh;
-@media ${device.mobile} {
-display: none    }
-
-`
+  text-align: center;
+  font-size: 4vh;
+  padding-left: 2vh;
+  @media ${device.mobile} {
+    display: none;
+  }
+`;
 export const PDescription = styled.p`
-text-align: center;
-font-size: 25px;
-@media ${device.mobile} {
-    display : none;
-    }
-`
+  text-align: center;
+  font-size: 25px;
+  @media ${device.mobile} {
+    display: none;
+  }
+`;
 
 export const ImgBan = styled.img`
-height: 5vh;
-background-repeat: repeat;
-width: 100%;
-background-color: rgba(239, 220, 190, 0.29);
-`
-
+  height: 5vh;
+  background-repeat: repeat;
+  width: 100%;
+  background-color: rgba(239, 220, 190, 0.29);
+`;
 
 export default function Header() {
-  return <div>
-  <DivHeader>
-
-      <DivLogo>
-          <a href="#3"><ImgLogo src="/images/logo.png" alt="logo"/></a>
+  return (
+    <div>
+      <DivHeader>
+        <DivLogo>
+          <a href="#3">
+            <ImgLogo src="/images/logo.png" alt="logo" />
+          </a>
           <section>
-          <PLogo>Blablachar
-          <PDescription>“The best way to travel around Rome”</PDescription>
-          </PLogo>
+            <PLogo>
+              Blablachar
+              <PDescription>“The best way to travel around Rome”</PDescription>
+            </PLogo>
           </section>
-      </DivLogo>
-      <A href="#1">Trips</A>
-      <A href="#2">Post a trip</A>
-      <A href="#3">Chariot</A>
-      
-        
-  </DivHeader>
-  <div>
-  <ImgBan src="/images/banniere.png" alt="banniere"/>
-  </div>
-  </div>;
+        </DivLogo>
+        <A to="drivers">Drivers</A>
+        <A to="post">Post a trip</A>
+        <A to="chariots">Chariots</A>
+      </DivHeader>
+      <div>
+        <ImgBan src="/images/banniere.png" alt="banniere" />
+      </div>
+    </div>
+  );
 }

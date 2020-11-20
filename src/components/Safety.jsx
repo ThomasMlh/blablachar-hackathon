b918f87413a889d74327ebaf1d2f516f38abbf02
Gauthier-Lhomme <lhomme.gaut@gmail.com>
2020-11-20 03:20:12 +0100

    Add css

--- src/components/Safety.jsx
+++ src/components/Safety.jsx
@@ -1,14 +1,29 @@
 import styled from "styled-components";
- 
- const Div = styled.div`
- background-color: rgba(239, 220, 190, 0.29);
- `
+
+const Div = styled.div`
+  background-color: rgba(239, 220, 190, 0.29);
+  text-align: center;
+  height: 66.5vh;
+`;
+
 const H1 = styled.h1`
-font-size: 3vh;
-`
-const H2 =styled.h2`
-font-size : 2.3vh
-`
+  font-size: 3vh;
+  padding-top: 2vh;
+  font-weight: bold;
+  font-family: cinzel decorative;
+`;
+
+const H2 = styled.h2`
+  font-size: 2.3vh;
+  padding-top: 2.6vh;
+  text-decoration: underline;
+  font-family: cinzel decorative;
+`;
+
+const P = styled.p`
+  padding-top: 1vh;
+  font-size: 2.4vh;
+`;
 
 export default function Safety() {
   return (
@@ -16,33 +31,33 @@ export default function Safety() {
       <H1>Safty tips for drivers</H1>
       <section>
         <H2>Avoid being too drunk on the road</H2>
-        <p>You might get to Rome ...</p>
+        <P>You might get to Rome ...</P>
       </section>
       <section>
         <H2>Don't forget your passengers</H2>
-        <p>Paymant is made on arrival (better alive).</p>
+        <P>Paymant is made on arrival (better alive).</P>
       </section>
       <section>
         <H2>Watch out for lion</H2>
-        <p>They run faster than horses.</p>
+        <P>They run faster than horses.</P>
       </section>
       <section>
         <H2>Don't use TikTok while driving</H2>
-        <p>Because TikTok sucks. </p>
+        <P>Because TikTok sucks. </P>
       </section>
       <section>
         <H2>
           Be carrefull with old people on the road (they might be your father)
         </H2>
-        <p>It would be such a shame to damage your wheels.</p>
+        <P>It would be such a shame to damage your wheels.</P>
       </section>
       <section>
         <H2>Feed your animals often</H2>
-        <p>A good advice if you want to finish the road on foot.</p>
+        <P>A good advice if you want to finish the road on foot.</P>
       </section>
       <section>
         <H2>Have the tire pressure checked</H2>
-        <p>It's 2.1 bar for heavy chariot and 2.3 var for light chariot.</p>
+        <P>It's 2.1 bar for heavy chariot and 2.3 var for light chariot.</P>
       </section>
     </Div>
   );
